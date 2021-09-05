17a9e6d4cefc8a81b6e2ddbc187bb76f662bcff8
Ymiee <xx770930@gmail.com>
2021-09-05 22:27:28 +0800

    Update 2021-09-05T14:27:10.459Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,25 +1,25 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
 });
-
-
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   centeredSlides: true,
   grid: {
-    row: 3,
+    row: 3
   },
   breakpoints: {
     slidesPerView: 3,
     spaceBetween: 30,
     // 768: {
-  //   //   slidesPerView: 1,
-  //   //   spaceBetween: 30,
-  //   // }
+    //   //   slidesPerView: 1,
+    //   //   spaceBetween: 30,
+    //   // }
     grid: {
-      row: 1,
-    },
+      row: 1
+    }
   }
 });
+//# sourceMappingURL=all.js.map
